b30f338e4dcecea12c555797f4de1eaac205c8f0
Krish <Krish>
2016-10-26 08:30:22 +0530

    full complete page

--- www/js/controllers/enterName.controller.js
+++ www/js/controllers/enterName.controller.js
@@ -3,7 +3,21 @@ mainApp.controller('enterNameController', ['$state', '$rootScope', '$scope', 'co
         var self = $scope;
         console.log('inside login controller');
         self.init = function () {
-            
+        };
+        self.submitUserName = function() {
+            console.log(self.userName);
+        };
+        self.submitButtonForAnimation = function($event) {
+            console.log(self.userName);
+            console.log($event);
+            console.log($event.offsetX);
+            console.log($event.offsetY);
+            $(".another-svg-image").css("right",$event.offsetX-$(".another-svg-image").width()/2);
+            $(".another-svg-image").css("bottom",$event.offsetY-$(".another-svg-image").height()/2);
+            $(".svg-2").animate({
+                width: '100px',
+                height: '100px'
+            },1000)
         };
     }
 ]);
